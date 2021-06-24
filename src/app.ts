ca17ec273e1078cca513f8e87a424f2adc321861
FoxGreedy <hotmus8500@gmail.com>
2021-06-23 21:20:48 -0300

    feat(app): upgrade exceptions treatment

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
 
 import { router } from 'routes'
 
 const app = express();
 
-app.use(express.json())
-app.use(router)
+app.use(express.json());
 
-export default app
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+
+    return response.status(500).json({ status: "error", message: "Internal Server Error" });
+})
+
+export default app;
--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,16 +3,12 @@ import { CreateUserService } from "services/CreateUserService";
 
 class CreateUserController {
     async handle(request: Request, response: Response) {
-        try {
-            const { name, email, admin } = request.body;
+        const { name, email, admin } = request.body;
 
-            const createUserService = new CreateUserService;
-            const user = await createUserService.execute({ name, admin, email });
+        const createUserService = new CreateUserService;
+        const user = await createUserService.execute({ name, admin, email });
 
-            return response.status(201).json(user);
-        } catch (error) {
-            return response.status(400).json({message: error.message})
-        }
+        return response.status(201).json(user);
     }
 }
 
